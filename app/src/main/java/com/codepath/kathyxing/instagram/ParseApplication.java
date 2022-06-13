fe98819ed49ca005baadadb60ef5d747059cb6bd
Kathy Xing <kathyxing@fb.com>
2022-06-13 15:19:28 -0700

    made login activity functional

--- app/src/main/java/com/codepath/kathyxing/instagram/ParseApplication.java
+++ app/src/main/java/com/codepath/kathyxing/instagram/ParseApplication.java
@@ -1,4 +1,22 @@
 package com.codepath.kathyxing.instagram;
 
-public class ParseApplication {
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    // Initializes Parse SDK as soon as the application is created
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
+    }
 }
