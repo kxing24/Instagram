07a348ae65e4c264c219c5cc9e721b2480dba315
Kathy Xing <kathyxing@fb.com>
2022-06-14 11:04:27 -0700

    added delay to splash screen

--- app/src/main/java/com/codepath/kathyxing/instagram/SplashScreenActivity.java
+++ app/src/main/java/com/codepath/kathyxing/instagram/SplashScreenActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // s̶e̶t̶C̶o̶n̶t̶e̶n̶t̶V̶i̶e̶w̶(̶)̶;
-        doFirstRunCheckup();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, SPLASH_TIMER);
+
     }
 
     private void doFirstRunCheckup() {
