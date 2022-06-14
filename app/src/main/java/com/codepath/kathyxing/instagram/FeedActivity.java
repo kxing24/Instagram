97d9f9cf6bdcf1b506a692cb76c80e02f52d2c16
Kathy Xing <kathyxing@fb.com>
2022-06-13 21:10:37 -0700

    added a recyclerview

---
+++ app/src/main/java/com/codepath/kathyxing/instagram/FeedActivity.java
@@ -0,0 +1,19 @@
+package com.codepath.kathyxing.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    private RecyclerView rvPosts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        rvPosts = findViewById(R.id.rvPosts);
+    }
+}
